614a2b69077730a1399a0030c0444c19a62a65c6
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-07-04 16:48:55 +0400

    allow additional div properties on the chapter component

--- packages/site-chapter/lib/main.tsx
+++ packages/site-chapter/lib/main.tsx
@@ -1,8 +1,10 @@
 import {type ChildrenIncludable} from "@onlyoffice/preact-types"
+import {type HTMLAttributes} from "preact/compat"
 import {type JSX, h} from "preact"
 
-export function Chapter({children}: ChildrenIncludable): JSX.Element {
-  return <div class="chapter">{children}</div>
+export function Chapter(p: HTMLAttributes<HTMLDivElement>): JSX.Element {
+  const {children, ...props} = p
+  return <div class="chapter" {...props}>{children}</div>
 }
 
 export function ChapterNavigation({children}: ChildrenIncludable): JSX.Element {
