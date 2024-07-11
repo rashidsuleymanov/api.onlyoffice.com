7352f9e95f4fc65fb08d3bda26baf8471dc14997
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-07-11 11:58:13 +0400

    fix the import of the table of contents in the table of contents layout

--- site/layouts/table-of-contents.tsx
+++ site/layouts/table-of-contents.tsx
@@ -1,18 +1,16 @@
 import {type Context, type Data} from "@onlyoffice/eleventy-types"
 import {Fragment, type JSX, h} from "preact"
-import {TableOfContents} from "@internal/table-of-contents.ts"
+import {TableOfContents} from "@/internal/table-of-contents.tsx"
 
 export function data(): Data {
   return {
-    layout: "chapter"
+    layout: "chapter",
   }
 }
 
 export function render(ctx: Context): JSX.Element {
-  return (
-    <>
-      {ctx.content}
-      <TableOfContents url={ctx.page.url} />
-    </>
-  )
+  return <>
+    {ctx.content}
+    <TableOfContents url={ctx.page.url} />
+  </>
 }
