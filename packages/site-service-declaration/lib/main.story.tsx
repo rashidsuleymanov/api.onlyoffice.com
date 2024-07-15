a53f22a3ed9b01bf479efb7f4963bb0d27d4fc5f
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-07-15 18:51:26 +0400

    temporarily disable service declaration stories

--- packages/site-service-declaration/lib/main.story.tsx
+++ packages/site-service-declaration/lib/main.story.tsx
@@ -1,28 +1,32 @@
-import {type ChildrenIncludable} from "@onlyoffice/preact-types"
-// todo: create resource.client.ts
-import {list, resolve} from "@onlyoffice/openapi-resource-fixtures/resource.client.ts"
-import {Content as UiContent} from "@onlyoffice/ui-kit"
+// todo: resuscitate stories
+// Add a step to generate service fixtures for the browser environment.
+
+// import {type ChildrenIncludable} from "@onlyoffice/preact-types"
+// import {list, resolve} from "@onlyoffice/openapi-resource-fixtures/resource.client.ts"
+// import {Content as UiContent} from "@onlyoffice/ui-kit"
 import {type Meta} from "@storybook/preact"
 import {Fragment, type JSX, h} from "preact"
-import {ServiceDeclaration} from "./main.tsx"
+// import {ServiceDeclaration} from "./main.tsx"
 
 export default {
-  title: "Site/Service Declaration"
+  title: "Site/Service Declaration",
 } satisfies Meta
 
 export function Default(): JSX.Element {
-  return <ServiceDeclaration
-    declaration={list()[1]}
-    onHighlightSyntax={Nop}
-    onRenderDescription={Nop}
-    onRetrieve={resolve}
-  />
+  return <></>
+  // return <ServiceDeclaration
+  //   declaration={list()[1]}
+  //   onHighlightSyntax={Nop}
+  //   onRenderDescription={Nop}
+  //   onRetrieve={resolve}
+  // />
 }
 
 export function Content(): JSX.Element {
-  return <UiContent><Default /></UiContent>
+  return <></>
+  // return <UiContent><Default /></UiContent>
 }
 
-function Nop({children}: ChildrenIncludable): JSX.Element {
-  return <>{children}</>
-}
+// function Nop(p: ChildrenIncludable): JSX.Element {
+//   return <>{p.children}</>
+// }
