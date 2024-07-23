4ba848c7a4dec272adef4f35bb2eff2ab0184876
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-07-23 12:15:16 +0400

    update breadcrumb stories

--- packages/ui-breadcrumb/lib/main.story.tsx
+++ packages/ui-breadcrumb/lib/main.story.tsx
@@ -3,13 +3,17 @@ import {type JSX, h} from "preact"
 import {Breadcrumb, BreadcrumbCrumb} from "./main.tsx"
 
 export default {
-  title: "UI/Breadcrumb"
+  title: "UI/Breadcrumb",
 } satisfies Meta
 
 export function Default(): JSX.Element {
   return <Breadcrumb aria-label="Breadcrumb">
-    <BreadcrumbCrumb href="/">Home</BreadcrumbCrumb>
-    <BreadcrumbCrumb href="/">Components</BreadcrumbCrumb>
-    <BreadcrumbCrumb href="/">Breadcrumb</BreadcrumbCrumb>
+    <BreadcrumbCrumb href="/">First</BreadcrumbCrumb>
+    <BreadcrumbCrumb href="/">Second</BreadcrumbCrumb>
+    <BreadcrumbCrumb href="/">Third</BreadcrumbCrumb>
+    <BreadcrumbCrumb href="/">In the middle</BreadcrumbCrumb>
+    <BreadcrumbCrumb href="/">Fourth</BreadcrumbCrumb>
+    <BreadcrumbCrumb href="/">Fifth</BreadcrumbCrumb>
+    <BreadcrumbCrumb href="/">Sixth</BreadcrumbCrumb>
   </Breadcrumb>
 }
