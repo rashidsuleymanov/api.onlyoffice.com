b7a683637a2fe7f276659e90928ef9f152e86359
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-08-02 17:12:12 +0400

    add site chapter container

--- packages/site-chapter/lib/main.tsx
+++ packages/site-chapter/lib/main.tsx
@@ -1,10 +1,11 @@
 import {type ChildrenIncludable} from "@onlyoffice/preact-types"
 import {type HTMLAttributes} from "preact/compat"
 import {type JSX, h} from "preact"
+import {type ChapterContainer} from "./client.ts"
 
-export function Chapter(p: HTMLAttributes<HTMLDivElement>): JSX.Element {
+export function Chapter(p: HTMLAttributes<ChapterContainer>): JSX.Element {
   const {children, ...props} = p
-  return <div class="chapter" {...props}>{children}</div>
+  return <chapter-container class="chapter" {...props}>{children}</chapter-container>
 }
 
 export function ChapterNavigation({children}: ChildrenIncludable): JSX.Element {
--- packages/site-kit/lib/client.ts
+++ packages/site-kit/lib/client.ts
@@ -1,3 +1,4 @@
+import "@onlyoffice/site-chapter/client.ts"
 import "@onlyoffice/site-document-editor-playground/client.ts"
 import "@onlyoffice/site-search/client.ts"
 import "@onlyoffice/site-theme-switcher/client.ts"
