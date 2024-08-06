a85f8921a9a7c2efd0d9bb8faa35b5c29a0cff49
vanyauhalin <ivan.uhalin@onlyoffice.com>
2024-08-06 18:59:08 +0400

    add link button component

--- packages/ui-button/lib/main.story.tsx
+++ packages/ui-button/lib/main.story.tsx
@@ -1,14 +1,19 @@
 import {type Meta} from "@storybook/preact"
 import {Fragment, type JSX, h} from "preact"
-import {Button} from "./main.tsx"
+import {Button, LinkButton} from "./main.tsx"
 
 export default {
   title: "UI/Button",
 } satisfies Meta
 
 export function Default(): JSX.Element {
+  const r = window.location.href
   return <>
+    <h2>Button</h2>
     <Button>Default</Button>
     <Button variant="accent">Accent</Button>
+    <h2>Link Button</h2>
+    <LinkButton href={r}>Default</LinkButton>
+    <LinkButton href={r} variant="accent">Accent</LinkButton>
   </>
 }
